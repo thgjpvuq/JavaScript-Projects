5b847f112d9cabde3c313561a59f00ef10a75b79
thgjpvuq <jpsa1016191@gmail.com>
2022-01-10 09:56:26 -0600

    added Project2_function
    added Project2_function to Basic_Javascript_Projects

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction()   {   // defining myFunction() //
+    var sentence = "i am learning"; // defining variable, sentence with a string //
+    sentence += " a lot";  // concatenating two strings //
+    document.getElementById("concatenate").innerHTML = sentence;    // returning the sentence variable with the concatenated strings //
+}
