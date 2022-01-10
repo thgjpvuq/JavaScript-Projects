f6baeeda8e0300431eb77684f1a8b4cc5930811d
thgjpvuq <jpsa1016191@gmail.com>
2022-01-10 16:24:41 -0600

    added comparison operators to  main.js

--- Basic_JavaScript_Projects/Project6_comparisons_type_coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project6_comparisons_type_coercion/JS/main.js
@@ -7,3 +7,10 @@ function b()  {
     var B = -3E311;
     document.getElementById("inf").innerHTML = B;
 }
+
+document.write(10 > 2);
+document.write(10 < 2);
+console.log(2 + 8);
+console.log( 15 > 35);
+document.write(10 == 10);
+document.write(10 == 123);
