2fb9394b28f8ec1ccf4bf2e60fa9a1dcbe5e0a7b
thgjpvuq <jpsa1016191@gmail.com>
2022-01-14 11:10:47 -0600

    added Project10

---
+++ Basic_JavaScript_Projects/Project10_loops_arrays/JS/get_id_inner_html.js
@@ -0,0 +1,9 @@
+function array_Function()   {
+    var a= [];
+    a[0] = 'this ';
+    a[1] = 'is ';
+    a[2] = 'a ';
+    a[3] = 'string';
+    document.getElementById("Array").innerHTML = a[0] + a[1] 
+    + a[2] + a[3] + ".";
+}
--- Basic_JavaScript_Projects/Project10_loops_arrays/JS/main.js
+++ Basic_JavaScript_Projects/Project10_loops_arrays/JS/main.js
@@ -1,13 +1,55 @@
-function Call_Loop()    {
-    var counter = "";
-    var x = 1;
-    while (x < 20)  {
-        counter += "<br>" + x;
-        x++;
-    }
-    document.getElementById("Loop").innerHTML = counter;
-}
+// function Call_Loop()    {
+//     var counter = "";
+//     var x = 1;
+//     while (x < 20)  {
+//         counter += "<br>" + x;
+//         x++;
+//     }
+//     document.getElementById("Loop").innerHTML = counter;
+// }
 
-a = 'string';
-b = a.length;
-document.write(b);
+// a = 'string';
+// b = a.length;
+// document.write(b);
+
+// function constant_function()    {
+//     const fruit = {name:'banana', color:'yellow'};
+//     fruit.name = 'apple';
+//     fruit.size = 'large';
+//     document.getElementById("constant").innerHTML = "A(n) " + fruit.name + " is a " + fruit.size + " " + fruit.color + " fruit.";
+// }
+
+// var X = 100;
+// document.write(X);
+// {
+//     let X = 50;
+//     document.write(X);
+// }
+// document.write(X);
+
+// var X = 100;
+// document.write(X);
+// {
+//     var X = 50;
+//     document.write(X);
+// }
+// document.write(X);
+
+// var X = 100;
+// document.write(X);
+// function A()    {
+//     var X = 50;
+//     return X;
+// }
+// document.write(A());
+
+let car = {
+    make: 'Dodge ',
+    model: 'Viper ',
+    year: '2021 ',
+    color: 'red ',
+    description : function car_function() {
+        return 'The car is a ' + this.year + this.color + this.make + this.model;
+        }
+};
+document.getElementById("car_object").innerHTML = car.description(); // this code will not display if not deferred //
