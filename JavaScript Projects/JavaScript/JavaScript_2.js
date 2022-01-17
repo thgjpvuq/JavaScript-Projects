39b804c758cbae9f6649e85ac209494620f46f43
thgjpvuq <jpsa1016191@gmail.com>
2022-01-17 12:06:09 -0600

    fixed form

--- JavaScript Projects/JavaScript/JavaScript_2.js
+++ JavaScript Projects/JavaScript/JavaScript_2.js
@@ -1,11 +1,3 @@
-// function validateForm() {
-//     let x = document.forms['Contact']['name'].value;
-//     if (x == '')    {
-//         alert('name must be filled out');
-//         return false;
-//     }
-// }
-
 function validateForm() {
     let x = document.forms["myForm"]["fname"].value;
     if (x == "") {
